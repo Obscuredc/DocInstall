e7064e63db0f7d55510e661a5261f9a4d4d8307c
Obscuredc <77344064+Obscuredc@users.noreply.github.com>
2021-06-07 15:26:03 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,6 @@
+var d = document.getElementsByClassName('kix-appview-editor');
+d[0].style.backgroundImage = 'url(https://cdn.discordapp.com/attachments/699996496279502909/851539433660481586/1853716.png)';
+d[0].style.backgroundPosition = 'center';
+d[0].style.backgroundRepeat = 'no-repeat';
+d[0].style.backgroundSize = 'cover';
+//obscuredc on github :)
